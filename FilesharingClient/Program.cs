492b57a4cbd39ac1ecb4cd1997b36a77a31ebc09
JekaLion <djoni-01@mail.ru>
2018-05-30 18:24:09 +0600

    init

---
+++ FilesharingClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesharingClient
+{
+    class Program
+    {
+        private const string SERVER_IP = "127.0.0.1";
+        private const int SERVER_PORT = 3535;
+
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            //client.Connect(SERVER_IP, SERVER_PORT);
+            ServerConnect serverConnect = new ServerConnect();
+            serverConnect.StartWork(client);
+        }
+    }
+}
---
+++ FilesharingClient/ServerConnect.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesharingClient
+{
+    public class ServerConnect
+    {
+        public void StartWork(TcpClient client)
+        {
+            while (true)
+            {
+                Work(client);
+            }
+        }
+        public async void Work(TcpClient client)
+        {
+            using (var networkStream = client.GetStream())
+            {
+                while (true)
+                {
+                    string path = Console.ReadLine();
+                    FileInfo info = new FileInfo(path);
+
+                    FileTransport file = new FileTransport();
+                    file.FileName = info.Name;
+                    file.Expansion = info.Extension;
+                    file.Data = File.ReadAllBytes(path);
+
+                    string serialized = JsonConvert.SerializeObject(file);
+                    Console.WriteLine(serialized.Length);
+                    var buffer = Encoding.Default.GetBytes(serialized);
+                    await networkStream.WriteAsync(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
